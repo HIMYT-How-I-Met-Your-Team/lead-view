4eed861aa49072b8f9b5c2334159cd10e4dadf1c
BadNarrators <andreaatzei01@gmail.com>
2022-01-31 02:38:24 +0100

    Comments in table.js

--- scripts/table.js
+++ scripts/table.js
@@ -1,18 +1,19 @@
 
-var deleteEnabled = false;
-var downloadEnabled = false;
+var deleteEnabled = false;  	//keeps in check if the delete button has been clicked
+var downloadEnabled = false;	//keeps in check if the download button has been clicked
 
-
+//changes the deleteEnabled variable
 function toggleDelete() {
 	deleteEnabled = !deleteEnabled;
 	document.getElementById("deletionMessage").classList.toggle("hidden");
 }
+//changes the downloadEnabled variable
 function toggleDownload() {
 	downloadEnabled = !downloadEnabled;
 	document.getElementById("downloadMessage").classList.toggle("hidden");
 }
 
-
+//performs either (or both) the onClick functions if the variable are toggled
 function clickFunctions(id) {
 	if (deleteEnabled){
 		clickDelete(id);
@@ -22,12 +23,13 @@ function clickFunctions(id) {
 	}
 }
 
-
+//deletes a row from the database
 function clickDelete(id) {
 	window.location.href = "https://myesperto.gbsweb.online/gp2/wp-admin/admin.php?page=lead-view&delete="+id;
 	deleteEnabled = false;
 	document.getElementById("deletionMessage").classList.toggle("hidden");
 }
+//download a row from the list
 function clickDownload(id) {
 	var message = document.getElementById("hidden"+id).value;
 	console.log(id+": "+message);
@@ -36,7 +38,7 @@ function clickDownload(id) {
 	document.getElementById("downloadMessage").classList.toggle("hidden");
 }
 
-
+//downloads a txt file named -filename- and containing the text -text-
 function download(filename, text) {
 	var element = document.createElement('a');
 	element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
@@ -50,6 +52,7 @@ function download(filename, text) {
 	document.body.removeChild(element);
 }
 
+//loops all the values in a list and then downloads the concat version of the list
 function downloadAll(list){
 	var breaker = "_______________________________________________\n";
 	var message = breaker;
